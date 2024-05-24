9e0049b202d9f823250a6e5d875c1769818dbe4e
ehmaddd <ehmaddd@gmail.com>
2024-05-24 23:19:13 +0500

    Delete comments

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Replace the string below with your actual MongoDB connection string
 const mongoUri = 'mongodb+srv://ehmaddd:mongoahmad@cluster.mongodb.net/shopdb?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri);
@@ -21,7 +20,6 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to MongoDB:', err);
 });
 
-// Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
