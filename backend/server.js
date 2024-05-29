984286c7c077e1c89fc19a64659668d6cb692672
ehmaddd <ehmaddd@gmail.com>
2024-05-29 23:43:40 +0500

    Comment all mongo contents

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -8,17 +8,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const mongoUri = 'mongodb+srv://ehmaddd:mongoahmad@cluster.mongodb.net/shopdb?retryWrites=true&w=majority';
+// const mongoUri = 'mongodb+srv://ehmaddd:mongoahmad@cluster.mongodb.net/shopdb?retryWrites=true&w=majority';
 
-mongoose.connect(mongoUri);
+// mongoose.connect(mongoUri);
 
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
-});
+// mongoose.connection.on('connected', () => {
+//     console.log('Connected to MongoDB');
+// });
 
-mongoose.connection.on('error', (err) => {
-    console.log('Error connecting to MongoDB:', err);
-});
+// mongoose.connection.on('error', (err) => {
+//     console.log('Error connecting to MongoDB:', err);
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
