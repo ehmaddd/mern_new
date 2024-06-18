570389f28e68a92a074466c23c3388494ef80e31
ehmaddd <ehmaddd@gmail.com>
2024-06-18 22:47:01 +0500

    Fetch documents

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.get('/read', async (req, res) => {
     try {
         const db = await connectToMongo();
         const collection = db.collection('myCollection');
-//         const documents = await collection.find({}).toArray();
+        const documents = await collection.find({}).toArray();
 //         res.send(documents);
     } catch (err) {
 //         res.status(500).send('Failed to read documents from MongoDB');
