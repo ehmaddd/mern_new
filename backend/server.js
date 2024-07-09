315c174b010497fe5d8f516454744f28597bb8e4
ehmaddd <ehmaddd@gmail.com>
2024-07-09 19:42:31 +0500

    Console and send ids

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.get('/fetchid', async (req, res) => {
         const documents = await collection.find({}, { projection: { id: 1 } }).toArray();
         const ids = documents.map(doc => doc.id);
         console.log(ids);
-        // res.send(ids);
+        res.send(ids);
     } catch (err) {
         console.error(err);
         res.status(500).send('Failed to read documents from MongoDB');
