2d5a2fe15086da6de2779615758640cda3401093
ehmaddd <ehmaddd@gmail.com>
2024-07-31 00:41:36 +0500

    Populate catch block

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.get('/read', async (req, res) => {
         const documents = await collection.find({}).toArray();
         res.send(documents);
     } catch (err) {
+        res.status(500);
     }
 });
 
